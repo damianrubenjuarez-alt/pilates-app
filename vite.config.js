a69c87b479fdf1f59070821f87d99ebb5d9ba81f
Damian <damianrubenjuarez@gmail.com>
2026-09-19 15:56:08 -0300

    Fix: quitar crossorigin del CSS (Chrome Android)

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,20 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import path from 'path'
 
+// Plugin: quitar 'crossorigin' del HTML generado
+function removeCrossorigin() {
+  return {
+    name: 'remove-crossorigin',
+    transformIndexHtml(html) {
+      return html
+        .replace(/ crossorigin/g, '')
+        .replace(/<link rel="modulepreload"[^>]*>/g, '');
+    },
+  };
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), removeCrossorigin()],
   resolve: {
     alias: {
       '@': path.resolve(import.meta.dirname, './src'),
@@ -14,14 +26,8 @@ export default defineConfig({
       output: {
         codeSplitting: {
           groups: [
-            {
-              name: 'react-vendor',
-              test: /node_modules\/(react|react-dom|react-router-dom)/,
-            },
-            {
-              name: 'firebase-vendor',
-              test: /node_modules\/(firebase|@firebase)/,
-            },
+            { name: 'react-vendor', test: /node_modules\/(react|react-dom|react-router-dom)/ },
+            { name: 'firebase-vendor', test: /node_modules\/(firebase|@firebase)/ },
           ],
         },
       },
